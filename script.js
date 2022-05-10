23365f34d9991b18782ad0418e65bcc22e15d91e
ggreenn <hers573@hsd.k12.or.us>
2022-05-10 15:22:09 -0700

    adding JS scripts for onload and re size events

---
+++ script.js
@@ -0,0 +1,19 @@
+/* When the page loads, add an open class to nav if the screen is wide enough. */
+
+window.onload = function() {
+    var screenWidth = window.innerWidth; 
+    var molineBreakpoint = 530;
+    var globalNavbar = document.getElementById("global-nav");
+    // add 'open' class to the ul in the navbar if wider than the breakpoint
+    if (screenWidth > mobileBreakpoint) {
+        globalNavbar.classList.add("open");
+    }
+}
+window.onresize = function() {
+    if (screenWidth > mobileBreakpoint) {
+        globalNavbar.classList.add("open");
+}
+else {
+    (screenWidth > mobileBreakpoint) {
+        globalNavbar.classList.remove("open");
+}
